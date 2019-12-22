0ea03fa7a32bce75719deed72f8b608f48d9a547
Damrankomran <aydinemre28@gmail.com>
2019-12-22 23:59:48 +0300

    initial commit.

---
+++ src/main/java/com/mycompany/chatbotservice/application/ApplicationClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.chatbotservice.application;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/webservice")
+public class ApplicationClass extends Application{
+    
+}
---
+++ src/main/java/com/mycompany/chatbotservice/application/pojo/Question.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.chatbotservice.application.pojo;
+
+public class Question {
+    
+    String question;
+    
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
